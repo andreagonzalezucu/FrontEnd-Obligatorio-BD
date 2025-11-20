c567d7ad27958509bab2703f2197cbcfc56e0c4d
andreagonzalezucu <andrea.gonzalezr@correo.ucu.edu.uy>
2025-11-20 10:31:06 -0300

    si no es esto me retiro

--- front-obligatorio/app/principal/_layout.tsx
+++ front-obligatorio/app/principal/_layout.tsx
@@ -4,6 +4,7 @@ export default function PrincipalLayout() {
   return (
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="index" />
+      <Stack.Screen name="misReservas" />
       <Stack.Screen name="edificio/[edi]" />
       <Stack.Screen name="edificio/sala/[sal]" />
     </Stack>
